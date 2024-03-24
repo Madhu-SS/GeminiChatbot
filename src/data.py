1b7e8138729de3626f5da0325b7cbc0f502c8a64
Madhu-SS <148480625+Madhu-SS@users.noreply.github.com>
2024-03-24 13:19:45 +0530

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,7 +1,9 @@
 from langchain.document_loaders.url import UnstructuredURLLoader
 
 URLs=[
-    "https://www.potentiam.co.uk/"
+    "https://www.potentiam.co.uk/",
+    "https://www.potentiam.co.uk/about-us",
+    "https://blog.potentiam.co.uk/"
 ]
 
 def DataLoader():
